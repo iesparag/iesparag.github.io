c57621fd803e1a51e31016b7325524ca4c93c09f
Parag Jain <iesparagjain@gmail.com>
2023-09-26 23:55:00 +0530

    swiper

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -4,59 +4,61 @@ import Ncc from "../assets/N.C.C_certificate .jpg"
 import Goi from "../assets/GOI.jpg"
 import Headquarter from "../assets/Headquarters_Certificate.jpg"
 import masai from "../assets/masai_award.jpg"
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import figma from "../assets/figma.png";
 import bootstrap from "../assets/bootstrap.png"
+import "./Achievements.css"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import {  Pagination } from 'swiper/modules';
 
 
 const Achievements = () => {
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        pauseOnHover: true,
-        autoplay: true,
-      autoplaySpeed: 2000,
-      };
+  
   return (
-    <Box id='achievements' paddingTop={{base:"70px",sm:"70px",md:"100px",lg:"10px"}}>
-        <Box className='section-title' >
-           <Heading opacity="0.5" as="h2"  size="2xl" >Achievements</Heading>
-        </Box>
-        {/* below content */}
-        
-         <Box justifyContent="center" alignItems="center" w={{base:"80vw",md:"60vw",lg:"40vw"}} m="auto">
-         <Slider {...settings}>
-          <div>
-      <Box m="auto" h="350px" ><Image src={figma} alt="image" w="100%" h="100%" /></Box>
-      </div>
-      <div>
-      <Box m="auto" h="350px" ><Image src={bootstrap} alt="image" w="100%" h="100%" /></Box>
-      </div>
-      <div>
-      <Box m="auto"  h="350px" ><Image src={masai} alt="image" w="100%" h="100%"  /></Box>
-      </div>
-      <div >
-        <Box m="auto"  h="350px" ><Image src={Ncc} alt="image" w="100%" h="100%" /></Box>
-      </div>
-      <div>
-      <Box m="auto" h="350px" ><Image src={Headquarter} alt="image" w="100%" h="100%" /></Box>
-        
-      </div>
-      <div>
-      <Box m="auto" h="350px" ><Image src={Goi} alt="image" w="100%" h="100%" /></Box>
+    // effect={'coverflow'}
+    // grabCursor={true}
+    // centeredSlides={true}
+    // slidesPerView={5}
+    // slidesPerGroup={1}
+    // coverflowEffect={{
+    //   rotate: 50,
+    //   stretch: 0,
+    //   depth: 100,
+    //   modifier: 4,
+    //   slideShadows: true,
+    // }}
+    // pagination={true}
+    // modules={[Pagination]}
+    // className="mySwiper"
 
-      </div>
-      
-     
-    </Slider>
-         </Box>
-
-    </Box>
+    <Swiper 
+    spaceBetween={50}
+    slidesPerView={3}
+    onSlideChange={() => console.log('slide change')}
+    onSwiper={(swiper) => console.log(swiper)}
+    modules={[Pagination]}
+  >
+    {/* <SwiperSlide className='SwiperSlide'>
+      <img src={figma} />
+    </SwiperSlide> */}
+    <SwiperSlide>
+      <img src={bootstrap} className='SwiperSlide' />
+    </SwiperSlide>
+    <SwiperSlide>
+      <img src={masai} className='SwiperSlide'/>
+    </SwiperSlide>
+    <SwiperSlide>
+      <img src={Ncc} className='SwiperSlide'/>
+    </SwiperSlide>
+    <SwiperSlide>
+      <img src={Headquarter} className='SwiperSlide'/>
+    </SwiperSlide>
+    <SwiperSlide>
+      <img src={Goi} className='SwiperSlide' />
+    </SwiperSlide>
+  </Swiper>
   )
 }
 
